8ea813406b13cbe8d191402aa847e18387c922cf
Supova <no-reply@domain.com>
2025-09-25 23:54:39 -0700

    test(init): add unit test for CircularBuffer_Init

---
+++ tests/c/test_circular_buffer.c
@@ -0,0 +1,15 @@
+#include "circular_buffer.h"
+#include <assert.h>
+
+void test_CircularBuffer_Init(){
+    CircularBuffer cb;
+    CircularBuffer_Init(&cb);
+    assert(cb.head == 0);
+    assert(cb.tail == 0);
+    assert(cb.full == false);
+}
+
+int main() {
+    test_CircularBuffer_Init();
+    return 0;
+}
